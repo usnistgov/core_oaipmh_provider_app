f077ae731efeb14b8858e8777ac52561737fe844
Augustin <augustin.chini@nist.gov>
2020-04-17 11:36:34 -0400

    feat: update font-awesome 5.13.0
    Change-Id: I60a334b16720272e0c3afec0247553a2243bad2a

--- core_oaipmh_provider_app/menus.py
+++ core_oaipmh_provider_app/menus.py
@@ -2,10 +2,10 @@ from django.urls import reverse
 from menu import Menu, MenuItem
 
 sharing_children = (
-    MenuItem("Identity", reverse("admin:core_oaipmh_provider_app_identity"), icon="gear"),
+    MenuItem("Identity", reverse("admin:core_oaipmh_provider_app_identity"), icon="cog"),
     MenuItem("Metadata Formats",
              reverse("admin:core_oaipmh_provider_app_metadata_formats"), icon="book"),
-    MenuItem("Sets", reverse("admin:core_oaipmh_provider_app_sets"), icon="reorder"),
+    MenuItem("Sets", reverse("admin:core_oaipmh_provider_app_sets"), icon="bars"),
 )
 
 Menu.add_item(
