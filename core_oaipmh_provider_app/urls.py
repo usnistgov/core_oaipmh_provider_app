61ac7140c22a121c74813ca59e1fb66922ce295d
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-07-24 16:34:42 -0400

    fix: py3.12 regex issue
    Change-Id: I5ce0c292a68cccfe60575e3e7be7fc9486f94f1c

--- core_oaipmh_provider_app/urls.py
+++ core_oaipmh_provider_app/urls.py
@@ -8,7 +8,7 @@ from core_oaipmh_provider_app.views.user.views import OAIProviderView, get_xsd
 urlpatterns = [
     re_path(r"^rest/", include("core_oaipmh_provider_app.rest.urls")),
     re_path(
-        r"^XSD/(?P<title>.*)/(?:(?P<version_number>\d+)/)$(?i)",
+        r"^XSD/(?P<title>.*)/(?:(?P<version_number>\d+)/)$",
         get_xsd,
         name="core_oaipmh_provider_app_get_xsd",
     ),
